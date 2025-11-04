2167e2fb386057e7785b114977efecf472523c22
Leonid Polyakov <polyakovleo@outlook.com>
2025-11-04 14:37:06 +0100

    Refactor Program.cs and add Sum method
    Updated namespace to use curly braces for better structure. Removed redundant "Hello, World!" print statements in the `Main` method. Introduced variables `a` and `b` and added a call to a new `Sum` method. The `Sum` method calculates and prints the sum of two integers.

--- GIT/Program.cs
+++ GIT/Program.cs
@@ -1,14 +1,20 @@
-﻿namespace GIT
+﻿
+namespace GIT
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+
+            int a = 1;
+            int b = 2;
+            Sum(a, b);
+        }
+
+        private static void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
         }
     }
 }
